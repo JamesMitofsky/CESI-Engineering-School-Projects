6571cb613b8a44b1a756f2d329ca082f74dd13f6
James Mitofsky <JamesMitofsky@gmail.com>
2023-12-20 14:35:02 +0100

    Add CORS policy to the back, allowing fetches from the front

--- Back/index.js
+++ Back/index.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes/restaurantRoute');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/restaurantRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const dbUrl = "mongodb://localhost:27017/mini-projet_resto";
 
-async function connectDB(){
-    try{
-        await mongoose.connect(dbUrl);
-        console.log("MongoDB successfully connected");
-    } catch(err){
-        console.log(err.message);
-        process.exit(1);
-    }
+async function connectDB() {
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("MongoDB successfully connected");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 }
-const app = express()
+const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(8080, () => {
-    console.log("Server à l'écoute");
-})
+  console.log("Server à l'écoute");
+});
