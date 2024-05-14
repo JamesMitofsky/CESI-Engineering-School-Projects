a5cb1723b6b4cb55fd66e708a8a9f4d20c57a984
James Mitofsky <JamesMitofsky@gmail.com>
2024-05-14 16:44:50 +0200

    Android: correct button shape & improve copy.

--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/AddTaskScreen.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/AddTaskScreen.kt
@@ -1,14 +1,20 @@
 package com.example.cesi_android_app
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun AddTaskScreen(navController: NavController) {
-    // Your code here
-    Button(onClick = { navController.navigate(Screen.TaskListScreen.route) }) {
-        Text("Go back to Task List Screen")
+    Column(){
+        Text(text="Add Task", style = MaterialTheme.typography.headlineLarge)
+
+        Button(onClick = { navController.navigate(Screen.TaskListScreen.route) }) {
+            Text("View existing tasks")
+        }
     }
+
 }
--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
@@ -1,14 +1,18 @@
 package com.example.cesi_android_app
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun TaskListScreen(navController: NavController) {
-    // Your code here
-    Button(onClick = { navController.navigate(Screen.AddTaskScreen.route) }) {
-        Text("Go to Add Task Screen")
+    Column() {
+        Text(text = "Tasks", style = MaterialTheme.typography.headlineLarge)
+        Button(onClick = { navController.navigate(Screen.AddTaskScreen.route) }) {
+            Text("Add Task")
+        }
     }
 }
