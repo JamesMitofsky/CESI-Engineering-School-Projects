143f02710a8b858d9c60a023ec3a56a54ca222da
James Mitofsky <JamesMitofsky@gmail.com>
2024-05-14 13:40:17 +0200

    Android: MVP of 4 cards which equally share the screen space.

--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/MainActivity.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/MainActivity.kt
@@ -6,23 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cesi_android_app.ui.theme.CESIAndroidappTheme
+import androidx.compose.runtime.mutableStateOf
 
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +39,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CESIAndroidappTheme {
-                QuarterOfScreenCard("title", "subtitle")
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Row(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        QuarterOfScreenCard("1-changed", "subtitle", Modifier.weight(1f))
+                        QuarterOfScreenCard("2", "subtitle", Modifier.weight(1f))
+                    }
+                    Row(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        QuarterOfScreenCard("3-changed", "subtitle", Modifier.weight(1f))
+                        QuarterOfScreenCard("4", "subtitle", Modifier.weight(1f))
+                    }
+                }
             }
         }
     }
@@ -48,9 +70,8 @@ fun QuarterOfScreenCard(
 //    isDark: Boolean = false,
 ) {
     Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(200.dp),
+        modifier = modifier.fillMaxSize(),
+        shape = RoundedCornerShape(0.dp),
         // inspired by this: https://stackoverflow.com/a/77229005/5395435
         colors = CardDefaults.cardColors(
             containerColor = Color.DarkGray,
