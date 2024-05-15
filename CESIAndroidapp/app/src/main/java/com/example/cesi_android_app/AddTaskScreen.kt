174fd5bc8d1b99b1c247d4b5e1aa5055a0678dcc
James Mitofsky <JamesMitofsky@gmail.com>
2024-05-15 11:45:21 +0200

    Android: Add basic scaffold for design

--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/AddTaskScreen.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/AddTaskScreen.kt
@@ -1,40 +1,76 @@
 package com.example.cesi_android_app
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import java.util.UUID
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTaskScreen(navigateToTaskListScreen: () -> Unit, onTaskAdded: (Task) -> Unit) {
     var taskInput by remember { mutableStateOf("") }
 
-    Column(){
-        Text(text="Add Task", style = MaterialTheme.typography.headlineLarge)
 
-        OutlinedTextField(
-            value = taskInput,
-            onValueChange = { taskInput = it },
-            label = { Text("Task") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Add Task")
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            OutlinedTextField(
+                value = taskInput,
+                onValueChange = { taskInput = it },
+                label = { Text("Task") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
 
-        Button(onClick = {
-            onTaskAdded(Task(id = UUID.randomUUID(), description = taskInput, completed = false))
-            taskInput = ""
-            navigateToTaskListScreen()
-        }) {
-            Text("Submit Task")
-        }
+            Button(onClick = {
+                onTaskAdded(Task(id = UUID.randomUUID(), description = taskInput, completed = false))
+                taskInput = ""
+                navigateToTaskListScreen()
+            }) {
+                Text("Submit Task")
+            }
 
-        Button(onClick = navigateToTaskListScreen) {
-            Text("View existing tasks")
+            Button(onClick = navigateToTaskListScreen) {
+                Text("View existing tasks")
+            }
         }
     }
+
+
 }
--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,33 +14,62 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 import java.util.UUID
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskListScreen(navigateToAddTaskScreen: () -> Unit, tasks: List<Task>, onTaskStatusChange: (taskId: UUID, isChecked: Boolean) -> Unit) {
-    Column {
-        Text(text = "Task List", style = MaterialTheme.typography.headlineLarge)
-
-        tasks.forEach { task ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = task.description, style = MaterialTheme.typography.bodyMedium)
-                Checkbox(
-                    checked = task.completed,
-                    onCheckedChange = { isChecked -> onTaskStatusChange(task.id, isChecked) },
-                    colors = CheckboxDefaults.colors(
-                        checkmarkColor = MaterialTheme.colorScheme.onSurface,
-                        uncheckedColor = MaterialTheme.colorScheme.onSurface,
-                        checkedColor = MaterialTheme.colorScheme.primary
-                    )
-                )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = colorScheme.primaryContainer,
+                    titleContentColor = colorScheme.primary,
+                ),
+                title = {
+                    Text("Task List")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = navigateToAddTaskScreen) {
+                Icon(Icons.Filled.Add, "Add Task")
             }
         }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            // internal content
 
-        Button(onClick = navigateToAddTaskScreen) {
-            Text("Add Task")
+            tasks.forEach { task ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = task.description, style = MaterialTheme.typography.bodyMedium)
+                    Checkbox(
+                        checked = task.completed,
+                        onCheckedChange = { isChecked -> onTaskStatusChange(task.id, isChecked) },
+                        colors = CheckboxDefaults.colors(
+                            checkmarkColor = colorScheme.onSurface,
+                            uncheckedColor = colorScheme.onSurface,
+                            checkedColor = colorScheme.primary
+                        )
+                    )
+                }
+            }
         }
     }
 }
