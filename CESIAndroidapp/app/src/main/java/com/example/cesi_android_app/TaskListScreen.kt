d38ab6cb8b462eb2052d2c551787154a45d1be97
James Mitofsky <JamesMitofsky@gmail.com>
2024-05-15 10:04:53 +0200

    Android: fix: correct incorrect dependancies.

--- CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
+++ CESIAndroidapp/app/src/main/java/com/example/cesi_android_app/TaskListScreen.kt
@@ -3,17 +3,18 @@ package com.example.cesi_android_app
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import java.lang.reflect.Modifier
 
 @Composable
 fun TaskListScreen(navController: NavController, tasks: List<Task>) {
-    Column() {
-        Text(text = "Tasks", style = MaterialTheme.typography.headlineLarge)
+    Column {
+        Text(text = "Task List")
 
         tasks.forEach { task ->
             Row(
