cc56b475dac13db12f5ec880adb22cf4603432b9
James Mitofsky <JamesMitofsky@gmail.com>
2024-05-15 14:17:32 +0200

    Android: fix dependancies: continue removing libs.androidx.ui dependancies.

--- CESIAndroidapp/app/build.gradle.kts
+++ CESIAndroidapp/app/build.gradle.kts
@@ -59,7 +59,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
