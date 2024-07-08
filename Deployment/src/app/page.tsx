e821987a8745ead14ef41a9dc074e470b10bb198
James Mitofsky <JamesMitofsky@gmail.com>
2024-07-08 10:26:47 +0200

    Add prettier

--- Deployment/src/app/page.tsx
+++ Deployment/src/app/page.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Voilà, cela sera mon application.
+      <h1>My Application</h1>
+      <Image
+        src="/path/to/your/image.jpg"
+        alt="Your Image Description"
+        width={500}
+        height={300}
+      />
+      <input
+        type="text"
+        placeholder="Enter something..."
+        className="mt-4 p-2 border rounded"
+      />
+      <button className="mt-4 p-2 bg-blue-500 text-white rounded">
+        Submit
+      </button>
+      Voilà, cela sera mon application.
     </main>
   );
 }
